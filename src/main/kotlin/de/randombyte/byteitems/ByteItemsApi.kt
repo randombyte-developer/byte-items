23fe870506e7f695149992046e0b3e43b97e5464
randombyte-developer <apps.randombyte@gmail.com>
2017-11-10 19:54:44 +0100

    added helper method; new version

--- src/main/kotlin/de/randombyte/byteitems/ByteItemsApi.kt
+++ src/main/kotlin/de/randombyte/byteitems/ByteItemsApi.kt
@@ -4,4 +4,6 @@ import org.spongepowered.api.item.inventory.ItemStackSnapshot
 
 interface ByteItemsApi {
     fun getItem(id: String): ItemStackSnapshot?
+    fun getItemSafely(id: String, failMessage: String = "The byte-item '$id' could not be found!") =
+            getItem(id) ?: throw IllegalArgumentException(failMessage)
 }
